97a231d1c6193e56d8ec6a51aa4bcaf363c689e6
AdelinGabriel21 <116873929+AdelinGabriel21@users.noreply.github.com>
2025-10-21 12:25:13 +0300

    add done

--- Tema1_Ex3/src/Ex3.java
+++ Tema1_Ex3/src/Ex3.java
@@ -1,2 +1,24 @@
 public class Ex3 {
+    public int[] add(int[] a, int[] b) {
+        if  (a.length != b.length) {
+            throw new IllegalArgumentException("The numbers must be the same length");
+        }
+
+        int carry = 0;
+        int[] result = new int[a.length + 1];
+        for (int i = a.length - 1, j = result.length - 1; i >= 0; i--, j--) {
+            int sum = a[i] + b[i] + carry;
+            result[j] = sum % 10;
+            carry = sum / 10;
+        }
+
+        result[0] = carry;
+        if (result[0] == 0){
+            int[] temp = new int[a.length];
+            System.arraycopy(result, 1, temp, 0, a.length);
+            return temp;
+        } else {
+            return result;
+        }
+    }
 }
--- Tema1_Ex3/src/Main.java
+++ Tema1_Ex3/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] a1 = {1, 3, 0, 0, 0, 0, 0, 0, 0};
+        int[] a2 = {8, 7, 0, 0, 0, 0, 0, 0, 0};
         Ex3 ex3 = new Ex3();
+
+        System.out.println(Arrays.toString(a1) + " + " + Arrays.toString(a2) + " = " + Arrays.toString(ex3.add(a1, a2)));
     }
 }
